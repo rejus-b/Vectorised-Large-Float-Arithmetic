f016ba2952e83cc0fe67934f1296d517efd2aff2
Rejus <rejus.b@hotmail.co.uk>
2024-03-03 23:22:57 +0000

    Closer to working, but I have some issues in unpad

--- src/avxmpfr_add.c
+++ src/avxmpfr_add.c
@@ -19,9 +19,20 @@ void avxmpfr_add(mpfr_t rop, mpfr_t op1, mpfr_t op2, mpfr_rnd_t rnd, uint16_t pr
     // First allign the exponents of the numbers to be added and set rop exponent
     rop->_mpfr_exp = avxmpfr_exp_allign(op1, op2);
 
+//    mp_limb_t *limbs = (mp_limb_t *)op1->_mpfr_d; 
+//    print_binary(limbs, PRECISION_256);
+
     // Now pad the limbs of these numbers
     op1->_mpfr_d = avxmpfr_pad252(op1);
     op2->_mpfr_d = avxmpfr_pad252(op2);
+
+    printf("\n");
+    mp_limb_t *limbs = (mp_limb_t *)op1->_mpfr_d; 
+    print_binary(limbs, PRECISION_256);
+
+    printf("\n");
+    limbs = (mp_limb_t *)op2->_mpfr_d; 
+    print_binary(limbs, PRECISION_256);
 
     // Now you can add these numbers and assign to rop
     // Note that you have to create a set of packed integers for the AVX lanes
@@ -30,24 +41,50 @@ void avxmpfr_add(mpfr_t rop, mpfr_t op1, mpfr_t op2, mpfr_rnd_t rnd, uint16_t pr
     __m256i_u op1_avx = _mm256_set_epi64x(op1->_mpfr_d[0],  // The least significant AVX lane / MPFR limb
 					    op1->_mpfr_d[1],
 					    op1->_mpfr_d[2],
-					    op1->_mpfr_d[3]);
+					    op1->_mpfr_d[3]); 
     
+
+   
     __m256i_u op2_avx = _mm256_set_epi64x(op2->_mpfr_d[0],  // The least significant AVX lane / MPFR limb
 					    op2->_mpfr_d[1],
 					    op2->_mpfr_d[2],
 					    op2->_mpfr_d[3]);
 
+    printf("\n");
+    printf("\n");
+    hexdump_m256i(op1_avx, "op1");
+
+    printf("\n");
+    hexdump_m256i(op2_avx, "op2");
+
+
     // Now you can add these
     __m256i_u rop_avx = avx_add(op1_avx, op2_avx);
     
+    printf("\n");
+    printf("\n");
+    hexdump_m256i(rop_avx, "rop_avx");
+
+
     // Now assign them to the actual rop
-    rop->_mpfr_d[0] = rop_avx[0];
-    rop->_mpfr_d[1] = rop_avx[1];
-    rop->_mpfr_d[2] = rop_avx[2];
-    rop->_mpfr_d[3] = rop_avx[3];
+    rop->_mpfr_d[0] = rop_avx[3];
+    rop->_mpfr_d[1] = rop_avx[2];
+    rop->_mpfr_d[2] = rop_avx[1];
+    rop->_mpfr_d[3] = rop_avx[0];
+
+
+    printf("\n");
+    printf("\n");
+    limbs = (mp_limb_t *)rop->_mpfr_d; 
+    print_binary(limbs, PRECISION_256);
      
     // Finally unpad rop
     rop->_mpfr_d = avxmpfr_unpad252(rop);
+
+    printf("\n");
+    printf("\n");
+    limbs = (mp_limb_t *)rop->_mpfr_d; 
+    print_binary(limbs, PRECISION_256);
 }
 
 int main() 
@@ -75,7 +112,7 @@ int main()
 
 
     // I cant even get the default MPFR print to work right now ...
-    printf("\nmpfr_add print results:");
+//    printf("\nmpfr_add print results:");
     //char* result_str = mpfr_get_str(NULL, &result->_mpfr_exp, 10, 0, number1, MPFR_RNDN);
     //mpfr_printf("%.6Rf", number1);
    
@@ -87,7 +124,7 @@ int main()
 
 
     avxmpfr_add(result, number1, number2, MPFR_RNDN, PRECISION_256);
-   
+/*
     printf("\nAfter avxmpfr_add"); 
     printf("\nExp: %ld \n", mpfr_get_exp(result)); // (result)->_mpfr_exp);
     limbs = (mp_limb_t *)result->_mpfr_d; 
@@ -97,6 +134,6 @@ int main()
     //result_str = mpfr_get_str(NULL, &result->_mpfr_exp, 10, 0, result, MPFR_RNDN);
     //printf("%s", result_str);
     mpfr_printf("%.256Rf", result);
-
+*/
     return 0;
 }
