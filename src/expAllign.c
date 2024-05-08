9ce718731d699f1849231441ef68f743177c4b5f
Rejus <rejus.b@hotmail.co.uk>
2024-05-08 08:08:22 +0100

    AVX2 PARALLEL exponent alignment

--- src/expAllign.c
+++ src/expAllign.c
@@ -40,9 +40,104 @@ mpfr_exp_t avxmpfr_exp_allign(mpfr_t firstNum, mpfr_t secondNum, const uint16_t 
 		mpfr_swap(firstNum, secondNum);
 		mpfrSwap = 1;
     }
+	
 
     /* Now to move onto actually shifting */
+	
 
+#define AVX_SHIFT 1
+
+#ifdef AVX_SHIFT
+    // Check first if shift is equal to or less than 64 bits
+    mpfr_exp_t expDifference;    
+    expDifference = (secondNum)->_mpfr_exp - (firstNum)->_mpfr_exp;
+
+
+	// Now that you have the MPFR number, extract it into AVX2 for vectorising the shift
+	__m256i_u avx_first_num = _mm256_set_epi64x(firstNum->_mpfr_d[0],  // The least significant AVX lane / MPFR limb
+					firstNum->_mpfr_d[1],
+					firstNum->_mpfr_d[2],
+					firstNum->_mpfr_d[3]); 
+					
+	
+					
+    if (expDifference <= 64)
+    {
+		// First mask out the low bits
+		__m256i_u last_bit_mask = _mm256_set1_epi64x(expDifference);
+		__m256i_u last_bit = _mm256_and_si256(avx_first_num, last_bit_mask);
+		
+			//			// Shift the top lane for right shifting it
+			//			__m256i top_lane_mask = _mm256_set_epi64x(0xFFFFFFFFFFFFFFFF, 0x0, 0x0, 0x0);
+
+		
+		// Shift the bit right across lanes.
+		__m256i_u top_bit = _mm256_sll_epi64(last_bit, _mm_cvtsi32_si128(64-expDifference));
+		
+			//			// Shift the first lane of top bits by expDif bits
+			//			__m256i shifted_lane = _mm256_srli_epi64(_mm256_and_si256(data, top_lane_mask), expDifference);
+		
+		top_bit = _mm256_set_epi64x(top_bit[2],
+							top_bit[1],
+							top_bit[0],
+							top_bit[0] - expDifference);	// First lane is right shifted by expDifference
+							
+		avx_first_num = _mm256_srl_epi64(avx_first_num, _mm_cvtsi32_si128(expDifference));	// Shift each lane right by expDiff
+		avx_first_num = _mm256_or_si256(avx_first_num, top_bit);
+		
+		// Now set the exponent to the shifted value
+		(firstNum)->_mpfr_exp += expDifference;
+	}
+    // If a difference of greater than 64
+    else if (expDifference > 64) 
+    {
+		// Keep track of total times a whole limb has shifted
+		int limbShiftCount = 0;
+
+		while (expDifference > 64)
+		{
+			// First mask out the low bits
+			__m256i_u last_bit_mask = _mm256_set1_epi64x(expDifference);
+			__m256i_u last_bit = _mm256_and_si256(avx_first_num, last_bit_mask);
+			
+			// Shift the bit right across lanes.
+			__m256i_u top_bit = _mm256_sll_epi64(last_bit, _mm_cvtsi32_si128(64-expDifference));
+			top_bit = _mm256_set_epi64x(top_bit[2],
+								top_bit[1],
+								top_bit[0],
+								top_bit[0] - expDifference);	// First lane is right shifted by expDifference
+								
+			avx_first_num = _mm256_srl_epi64(avx_first_num, _mm_cvtsi32_si128(expDifference));	// Shift each lane right by expDiff
+			avx_first_num = _mm256_or_si256(avx_first_num, top_bit);
+			expDifference -= 64;
+			limbShiftCount++;	
+		}
+
+		// The difference should now be less than or equal to 64
+		// First mask out the low bits
+		__m256i_u last_bit_mask = _mm256_set1_epi64x(expDifference);
+		__m256i_u last_bit = _mm256_and_si256(avx_first_num, last_bit_mask);
+		
+		// Shift the bit right across lanes.
+		__m256i_u top_bit = _mm256_sll_epi64(last_bit, _mm_cvtsi32_si128(64-expDifference));
+		top_bit = _mm256_set_epi64x(top_bit[2],
+							top_bit[1],
+							top_bit[0],
+							top_bit[0] - expDifference);	// First lane is right shifted by expDifference
+							
+		avx_first_num = _mm256_srl_epi64(avx_first_num, _mm_cvtsi32_si128(expDifference));	// Shift each lane right by expDiff
+		avx_first_num = _mm256_or_si256(avx_first_num, top_bit);
+
+		// Now set the exponent to the shifted value
+		(firstNum)->_mpfr_exp += GMP_NUMB_BITS * limbShiftCount + expDifference;
+    }
+
+#endif
+
+
+
+
+#ifndef AVX_SHIFT
     // Check first if shift is equal to or less than 64 bits
     mpfr_exp_t expDifference;    
     expDifference = (secondNum)->_mpfr_exp - (firstNum)->_mpfr_exp;
@@ -62,20 +157,22 @@ mpfr_exp_t avxmpfr_exp_allign(mpfr_t firstNum, mpfr_t secondNum, const uint16_t 
 		// Keep track of total times a whole limb has shifted
 		int limbShiftCount = 0;
 
-	while (expDifference > 64)
-	{
-	    mpn_rshift((firstNum)->_mpfr_d, (firstNum)->_mpfr_d, (PRECISION + GMP_NUMB_BITS - 1) / GMP_NUMB_BITS, 64);
-	    expDifference -= 64;
-	    limbShiftCount++;	
-	}
+		while (expDifference > 64)
+		{
+			mpn_rshift((firstNum)->_mpfr_d, (firstNum)->_mpfr_d, (PRECISION + GMP_NUMB_BITS - 1) / GMP_NUMB_BITS, 64);
+			expDifference -= 64;
+			limbShiftCount++;	
+		}
 
-	// The difference should now be less than or equal to 64
-	mpn_rshift((firstNum)->_mpfr_d, (firstNum)->_mpfr_d, (PRECISION + GMP_NUMB_BITS - 1) / GMP_NUMB_BITS, expDifference);
+		// The difference should now be less than or equal to 64
+		mpn_rshift((firstNum)->_mpfr_d, (firstNum)->_mpfr_d, (PRECISION + GMP_NUMB_BITS - 1) / GMP_NUMB_BITS, expDifference);
 
-	// Now set the exponent to the shifted value
-	(firstNum)->_mpfr_exp += GMP_NUMB_BITS * limbShiftCount + expDifference;
+		// Now set the exponent to the shifted value
+		(firstNum)->_mpfr_exp += GMP_NUMB_BITS * limbShiftCount + expDifference;
     }	
-    
+    #endif
+	
+	/* Code runs for both AVX and linear */
     /* Allignment complete */
 
     // If the mpfr_t variables were swapped, swap them back
@@ -87,6 +184,7 @@ mpfr_exp_t avxmpfr_exp_allign(mpfr_t firstNum, mpfr_t secondNum, const uint16_t 
 
     return secondExp;
 }
+
 
 /*
 int main() {
